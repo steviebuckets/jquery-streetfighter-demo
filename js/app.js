39b4068bf5fb65c089fa4694fb087eb5a6acd2ab
Stevie Buckets <steviebuckets33@gmail.com>
2016-04-06 17:14:43 -0700

    modified js/app.js

--- js/app.js
+++ js/app.js
@@ -8,12 +8,14 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    console.log('mousedown');
     // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
     // show hadouken and animate it to the right of the screen
   })
   .mouseup(function() {
-    console.log('mouseup');
-    // ryu goes back to his ready position
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
 });
